90fea59baf46457c582aeaf8c2b4e72a1ad96efc
gusarov2k <motoface@yandex.ru>
2018-08-01 01:09:57 +0300

    add new test edit

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -54,4 +54,16 @@ RSpec.describe QuestionsController, type: :controller do
 		end
 	end
 
+	describe 'GET #edit' do
+		let(:question) { create(:question) }
+		before { get :show, id: question }
+
+		it 'assings the requested question to @question'do
+			expect(assigns(:question)).to eq question
+		end
+
+		it 'renders edit view' do
+			expect(response).to render_template :new
+		end
+	end
 end
