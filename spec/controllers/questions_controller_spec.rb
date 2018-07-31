aff9345223f686b59d16d8dcce2a3c689962b27b
gusarov2k <motoface@yandex.ru>
2018-08-01 00:37:48 +0300

    refectoring 2 test controller

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -2,20 +2,25 @@ require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
 	describe 'GET #index' do
+
+		before do
+			@questions = FactoryBot.create_list(:question, 2)
+			# Делаем запрос
+			get :index
+		end
 		# Заполняте массив все вопросов
 		it 'populates an array of all questions' do
-			questions = FactoryBot.create_list(:question, 2)
+			
 	
 
-			get :index
+			
 			# Провереяем что в instans varibale :questions есть значения ниших созданные ранее переменных
-			expect(assigns(:questions)).to match_array(questions)
+			expect(assigns(:questions)).to match_array(@questions)
 		end
 
 		# отрендерить index
 		it 'renders index view' do
-			# Делаем запрос
-			get :index
+			
 			# Ожидаем что наш ответ от контроллера такой же как шаблон :index (render_template :index)
 			expect(response).to render_template :index
 		end
