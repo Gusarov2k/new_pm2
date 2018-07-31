0b5fbbc895408dff28b8c05c06e80e30dd9eaf62
gusarov2k <motoface@yandex.ru>
2018-07-31 23:35:16 +0300

    add test for controllers

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
+	describe 'GET #index' do
+		# Заполняте массив все вопросов
+		it 'populates an array of all questions' do
+			questions1 = FactoryBot.create(:question)
+			questions2 = FactoryBot.create(:question)
+
+			get :index
+			# Провереяем что в instans varibale :questions есть значения ниших созданные ранее переменных
+			expect(assigns(:questions)).to match_array([questions1, questions2])
+		end
+
+		# отрендерить index
+		it 'renders index view' do
+			# Делаем запрос
+			get :index
+			# Ожидаем что наш ответ от контроллера такой же как шаблон :index (render_template :index)
+			expect(response).to render_template :index
+		end
+	end
 
 end
