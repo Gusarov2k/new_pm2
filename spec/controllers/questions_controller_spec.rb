a9d964eb508509d74c96e384bb509f555435b78d
gusarov2k <motoface@yandex.ru>
2018-08-01 01:12:44 +0300

    add method new and view test good

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -56,14 +56,14 @@ RSpec.describe QuestionsController, type: :controller do
 
 	describe 'GET #edit' do
 		let(:question) { create(:question) }
-		before { get :show, id: question }
+		before { get :edit, id: question }
 
 		it 'assings the requested question to @question'do
 			expect(assigns(:question)).to eq question
 		end
 
 		it 'renders edit view' do
-			expect(response).to render_template :new
+			expect(response).to render_template :edit
 		end
 	end
 end
